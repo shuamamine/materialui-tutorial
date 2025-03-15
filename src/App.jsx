98cb11d5ed88219519e2037b0c615df19369c1c5
shuamamine <sahasenjuti796@gmail.com>
2025-03-16 02:56:46 +0530

    typography, icons, custom themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Note from './components/Note'
 import Create from './components/Create'
-
+import { createTheme, ThemeProvider } from '@mui/material/styles'; 
+import { purple } from '@mui/material/colors';
+const theme = createTheme({ 
+  palette: {
+    primary: { main: '#fefefe' },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+});
 function App() {
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Router>
         <Routes>
           <Route exact path="/" element={<Note />}/>
           <Route path="/create" element={<Create />} />
         </Routes> 
       </Router>
+      </ThemeProvider>
     </>
   )
 }
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,23 +1,49 @@
-import * as React from 'react'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+// import AcUnitIcon from '@mui/icons-material/AcUnit';
+
 export default function Create() {
-    
-    return ( 
-        <>
-             <div>
-                 <Typography color="secondary" variant="h5" align="center" gutterBottom>
-                    Create a New Note
-                 </Typography> 
-                 <Button variant="contained" color="success" type="submit" onClick={() => {
-    alert('clicked');
-  }}>Submit</Button>        
-            </div> 
-        </>   
-      );
+
+  return (
+    <>
+      <div>
+        <Container maxWidth="sm">
+          <Typography
+            sx={{ textDecoration: "underline", marginBottom: 2 }}
+            color="secondary"
+            variant="h5"
+            align="center"
+            gutterBottom
+          >
+            Create a New Note
+          </Typography>
+          <Button
+            sx={{
+              fontSize: 20,
+              backgroundColor: "violet",
+              "&:hover": { backgroundColor: "blue" },
+            }}
+            variant="contained"
+            type="submit"
+            endIcon={<KeyboardArrowRightIcon />}
+            onClick={() => {
+              alert("clicked");
+            }}
+          >
+            Submit
+          </Button>
+          <br />
+          {/* <AcUnitIcon />
+  <AcUnitIcon color="secondary" fontSize="large" /> */}
+        </Container>
+      </div>
+    </>
+  );
 }
- 
